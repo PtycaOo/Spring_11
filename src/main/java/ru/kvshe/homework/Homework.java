b25e574e1a006f55cbc980b0278d7e9495aa91de
DenKu <Denkuzia5155@gmail.com>
2024-11-11 16:43:53 +0300

    #1

---
+++ src/main/java/ru/kvshe/homework/Homework.java
@@ -0,0 +1,13 @@
+package ru.kvshe.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework.class, args);
+    }
+
+}
